ba1e73df3dec99d5dcc5798b89d3dd6dee27857a
Lasha Kvantaliani <lasharela@gmail.com>
2023-10-17 18:18:28 -0700

    add social media tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* icons */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -29,6 +30,23 @@ export default function Document() {
         />
         <meta name="msapplication-TileColor" content="#C4C6E7" />
         <meta name="theme-color" content="#ffffff" />
+        {/* social media open graph */}
+        <meta
+          property="og:title"
+          content="Sweenk - Personalized AI News Assistant"
+        />
+        <meta property="og:site_name" content="Sweenk" />
+        <meta property="og:url" content="https://sweenk.com" />
+        <meta
+          property="og:description"
+          content="Discover Sweenk: The future of news consumption. Engage with personalized, AI-curated news through interactive conversations. No more long articles, just meaningful news dialogues tailored to your interests."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="images/social-media/share-thumbnail.png"
+        />
+
         <title>Sweenk - Personalized AI News Assistant</title>
       </Head>
       <body x-data="{ page: 'home', 'loaded': true, 'stickyMenu': false, 'navigationOpen': false, 'scrollTop': false }">
