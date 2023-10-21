b001e8576e747e31822e6c2a8f1e5e6b449c7a5c
Lasha Kvantaliani <lasharela@gmail.com>
2023-10-21 09:39:58 -0500

    support

---
+++ src/pages/support/index.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "@/components";
+
+export default function PrivacyPage() {
+  return (
+    <Layout title="Support">
+      For support please email us: support@sweenk.com
+    </Layout>
+  );
+}
